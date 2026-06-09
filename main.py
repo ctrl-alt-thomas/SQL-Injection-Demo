c7f7a0768081fb9006a9aa5f2211766e5ec904b3
ctrl-alt-thomas <208521151+ctrl-alt-thomas@users.noreply.github.com>
2026-06-09 14:39:03 +0200

    Do not print error messages

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ conn = sqlite3.connect('database.db')
 
 
 while True:
-    user = input("Enter your username: ")
-    password = input("Enter your password: ")
-    cur = conn.cursor()
-    cur.execute(f"SELECT id, username FROM users WHERE username = '{user}' AND password = '{password}'")
-    match = cur.fetchone()
+    try:
+      user = input("Enter your username: ")
+      password = input("Enter your password: ")
+      cur = conn.cursor()
+      cur.execute(f"SELECT id, username FROM users WHERE username = '{user}' AND password = '{password}'")
+      match = cur.fetchone()
 
-    if match is None:
-        print("You are a faker.")
-    else:
-        print(f"Welcome {match[1]}!")
+      if match is None:
+          print("You are a faker.")
+      else:
+          print(f"Welcome {match[1]}!")
+    except:
+        print("Internal Error: Something went wrong! Try again.")
